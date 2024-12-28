f1876c41a4ff64458f0e35cc2b4702c0f94ab416
Norbert Takacs <norbert.takacs@postnord.com>
2024-12-28 19:13:09 +0100

    giscus fix

--- src/components/Giscus/index.tsx
+++ src/components/Giscus/index.tsx
@@ -9,23 +9,30 @@ export const GiscusComments = () => {
   return (
     <Fragment>
       <Gutter>
-        <div style={{ marginTop: '2rem' }}>
-          <h5>Join the Discussion on github</h5>
-          <Giscus
-            id="comments"
-            repo="bokker/tnorbert-blog"
-            repoId="R_kgDOM-_PCA"
-            category="Announcements"
-            categoryId="DIC_kwDOM-_PCM4ClbMt"
-            mapping="pathname"
-            term="Github discussion module"
-            reactionsEnabled="0"
-            emitMetadata="0"
-            inputPosition="top"
-            theme={theme}
-            lang="en"
-            loading="lazy"
-          />
+        <div className="flex justify-center">
+          <div
+            className="justify-center flex flex-1 flex-col pt-8"
+            style={{
+              maxWidth: 800,
+            }}
+          >
+            <h5>Join the Discussion on github</h5>
+            <Giscus
+              id="comments"
+              repo="bokker/tnorbert-blog"
+              repoId="R_kgDOM-_PCA"
+              category="Announcements"
+              categoryId="DIC_kwDOM-_PCM4ClbMt"
+              mapping="pathname"
+              term="Github discussion module"
+              reactionsEnabled="0"
+              emitMetadata="0"
+              inputPosition="top"
+              theme={theme}
+              lang="en"
+              loading="eager"
+            />
+          </div>
         </div>
       </Gutter>
     </Fragment>
