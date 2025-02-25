2d647e84e8f7355679ac67d1efe21583f9edc94d
Norbert Takacs <norbert.takacs@postnord.com>
2025-02-25 16:40:01 +0100

    test url config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,13 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+const formatURL = (url) => {
+  if (!url) return undefined
+  return url.startsWith('http') ? url : `https://${url}`
+}
+
+const NEXT_PUBLIC_SERVER_URL = formatURL(process.env.VERCEL_PROJECT_PRODUCTION_URL)
+  ? formatURL(process.env.VERCEL_PROJECT_PRODUCTION_URL)
   : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
