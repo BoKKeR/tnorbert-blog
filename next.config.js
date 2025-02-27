c16710579dac97b757abdea89b40cd5f38b28b63
Norbert Takacs <norbert.takacs@postnord.com>
2025-02-27 10:19:26 -0600

    added url

--- next.config.js
+++ next.config.js
@@ -7,23 +7,25 @@ const formatURL = (url) => {
   return url.startsWith('http') ? url : `https://${url}`
 }
 
-const NEXT_PUBLIC_SERVER_URL = 'http://localhost:3000'
-// const NEXT_PUBLIC_SERVER_URL = formatURL(process.env.VERCEL_PROJECT_PRODUCTION_URL)
-//   ? formatURL(process.env.VERCEL_PROJECT_PRODUCTION_URL)
-//   : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+// const NEXT_PUBLIC_SERVER_URL = 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL = formatURL(process.env.VERCEL_PROJECT_PRODUCTION_URL)
+  ? formatURL(process.env.VERCEL_PROJECT_PRODUCTION_URL)
+  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
+      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */, 'http://localhost:3000'].map(
+        (item) => {
+          const url = new URL(item)
 
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
+          return {
+            hostname: url.hostname,
+            protocol: url.protocol.replace(':', ''),
+          }
+        },
+      ),
     ],
   },
   reactStrictMode: true,
