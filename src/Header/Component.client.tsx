3eb415250bb5143295dd141b3657ce03693c3cad
Norbert Takacs <norbert.takacs@postnord.com>
2025-05-07 22:22:36 +0200

    added more printing css

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -30,7 +30,10 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   }, [headerTheme])
 
   return (
-    <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
+    <header
+      className="print:hidden container relative z-20   "
+      {...(theme ? { 'data-theme': theme } : {})}
+    >
       <div className="py-8 flex justify-between">
         <Link href="/">
           <div style={{ position: 'absolute', top: 20 }}>
--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -62,7 +62,7 @@ export default async function Post({ params: paramsPromise }: Args) {
 
       <PostHero post={post} />
 
-      <div className="flex flex-col items-center gap-4 pt-8">
+      <div className="flex flex-col items-center gap-4 pt-8 print:gap-0 print:pt-0">
         <div className="container">
           <RichText className="max-w-[48rem] mx-auto" data={post.content} enableGutter={false} />
           <SubstackEmail />
